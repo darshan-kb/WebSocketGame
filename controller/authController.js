3ba7f7c3fe013afe767ad1eb6459a0d02f444c19
Darshan <darshan.kb@outlook.com>
2023-03-24 17:02:29 +0530

    password hashing is applied

--- controller/authController.js
+++ controller/authController.js
@@ -2,6 +2,21 @@ const db = require("../config/db.js");
 const User = require("../model/User.js");
 const handleErrors = (err) => {
     console.log(err.message, err.code)
+    let errors = {email:'', password:''};
+
+    if(err.code === 11000){
+        errors.email = 'That email is already registered';
+        return errors;
+    }
+
+    //validation errors
+    if(err.message.includes('user validation failed')){
+        Object.values(err.errors).forEach(({properties})=>{
+            errors[properties.path] = properties.message;
+        });
+    }
+
+    return errors;
 }
 
 const items = [
@@ -35,8 +50,8 @@ module.exports.signup_post = async (req, res) => {
         res.status(201).json(user);
     }
     catch(err){
-        handleErrors(err);
-        res.status(400).send('error, user not created');
+        const errors = handleErrors(err);
+        res.status(400).json({errors});
     }
     //res.send('signup');
 }
--- model/User.js
+++ model/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
+
 const userSchema = new mongoose.Schema({
     email:{
         type: String,
@@ -13,6 +15,13 @@ const userSchema = new mongoose.Schema({
         require: [true,'Please enter an password'],
         minlength: [6,'Minimum password length is 6 character']
     },
+});
+
+//fire a function before doc saved to db
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password,salt);
+    next();
 });
 
 const User = mongoose.model('user', userSchema);
